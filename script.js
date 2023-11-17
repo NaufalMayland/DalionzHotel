74b34c6b581921a9b5b81aaeb1925ce9b4a49e4f
NaufalMayland <maylandzaa13@gmail.com>
2023-11-17 10:43:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function register() {
+    document.getElementById("signup").style.display = "block";
+}
+function close() {
+    document.getElementById("popup").style.display = "none";
+}
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
